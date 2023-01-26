3b509e38eadd821568dfd9d4181814a34851b4e7
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-01-26 11:23:36 +1100

    test frontmatter syntax

--- src/components/Quiz/Question.vue
+++ src/components/Quiz/Question.vue
@@ -4,34 +4,64 @@
   >
     <div class="p-4">
       <h4>{{ label }}</h4>
-      <slot id="my-id" />
+
+      <Option v-for="a in options" :key="a.id" :label="a.label" :explain="a.explain" :id="a.id" />
+
+      <!-- <div ref="el">
+        <slot />
+      </div> -->
+
       <button
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Submit
       </button>
-
-      <!-- <template slot-scope="{ signal }">
-        <option :signal="'foo'"></option>
-      </template> -->
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import Option from './OptionDetail.vue';
 import { useSlots, ref, computed, onMounted } from 'vue';
-
+// const el = ref();
 const slots = useSlots();
-const props = defineProps({ label: String });
+defineProps({ label: String });
+const options = ref<Option[]>([]);
 
-const slotText = computed(() => {
-  return typeof slots && slots.default ? slots.default()[0].children : null; // This is the interesting line
-});
+interface Option {
+  label: string;
+  isAnswer?: boolean;
+  xp?: string;
+  explain?: string;
+  id: string;
+}
+
+// const slotText = computed(() => {
+//   return typeof slots && slots.default ? slots.default()[0].children : null; // This is the interesting line
+// });
 onMounted(() => {
-  // console.log('slots.default()', slots.default());
+  // console.log('el.value)', el.value);
+  if (slots?.default) {
+    slots?.default().map((vNode) => {
+      const strObj = vNode?.props?.value.trim();
+      if (strObj) {
+        const OptionsStr = `[${strObj}]`;
+        try {
+          const list = JSON.parse(OptionsStr.replace('},]', '}]'));
+          options.value = list.map((o, idx) => {
+            o.id = idx;
+            return o;
+          });
+        } catch (error) {}
+      }
+      // if (!vNode.componentOptions) return false;
+      // return Boolean(vNode.componentOptions.propsData.active);
+    });
+  }
+
+  // console.log('slots.default()', slots.default()[0].props.value);
   // slots.default().map(vnode => (vnode.text || vnode.elm.innerText)).join('');
-  console.log('slotText', slotText);
-  console.log('props', props);
+  // console.log('slotText', slotText);
 });
 </script>
