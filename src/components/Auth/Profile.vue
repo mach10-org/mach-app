e58b58166f07d93432e4fd276bac050bf1b9fb4d
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-02-01 11:57:15 +1100

    profile questions

--- src/components/Auth/Profile.vue
+++ src/components/Auth/Profile.vue
@@ -2,15 +2,19 @@
   <section>
     <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto">
       <div
-        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 xl:p-0 dark:bg-gray-800 dark:border-gray-700"
       >
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h2 class="text-center text-xl font-bold text-gray-900 dark:text-white">
             {{ user?.email }}
           </h2>
-          <h3 class="text-center text-lg font-bold text-gray-900 dark:text-white">
-            XP points <br />{{ xp }}
-          </h3>
+          <h3 class="text-center text-lg font-bold text-gray-900 dark:text-white">{{ xp }} XP</h3>
+          <h4>
+            We’d love to learn more about you. Could you share with us what is your main goal
+            applying here? Even if it’s just curiosity, we’re interested to hear ☺. <br />This is
+            also helping us to know our audience and adapt our content accordingly.
+          </h4>
+
           <p class="text-center mb-2 font-light text-gray-500 dark:text-gray-400">
             Update your profile !
           </p>
@@ -18,26 +22,166 @@
           <form class="space-y-4 md:space-y-6">
             <div>
               <label
-                for="email"
+                for="full_name"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >Username</label
+                >Your firstname (how should we call you?)</label
               >
               <input
                 type="text"
-                name="userName"
-                id="username"
-                v-model="username"
+                v-model="full_nameModel"
+                id="full_name"
+                name="full_name"
                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="my username"
-                required
+                placeholder="firstname"
               />
             </div>
 
+            <div>
+              <label
+                for="gender"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Gender</label
+              >
+
+              <select
+                id="gender"
+                name="gender"
+                v-model="genderModel"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option value="">Choose</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+
+            <div>
+              <label for="dob" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Birthdate</label
+              >
+
+              <div class="relative">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    aria-hidden="true"
+                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+                <input
+                  datepicker
+                  id="dob"
+                  name="dob"
+                  v-model="dobModel"
+                  type="date"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Select date"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                for="education"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Education level</label
+              >
+
+              <select
+                id="education"
+                name="education"
+                v-model="educationModel"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option value="">Choose</option>
+                <option value="Primary education">Primary education</option>
+                <option value="Secondary education">Secondary education</option>
+                <option value="High education">High education</option>
+                <option value="Master’s">Master’s</option>
+              </select>
+            </div>
+
+            <div>
+              <label
+                for="computer_xp"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Experience with computers</label
+              >
+
+              <select
+                id="computer_xp"
+                name="computer_xp"
+                v-model="computer_xpModel"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option value="">Choose</option>
+                <option value="Never used it">Never used it</option>
+                <option value="I know how to use a computer but never did technical work">
+                  I know how to use a computer but never did technical work
+                </option>
+                <option value="I have already programming experience">
+                  I have already programming experience
+                </option>
+              </select>
+            </div>
+
+            <div>
+              <label for="goal" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >What is your goal with this program?</label
+              >
+              <div class="grid grid-cols-2 gap-2">
+                <div
+                  v-for="(value, index) in goalsList"
+                  class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700"
+                >
+                  <input
+                    :key="index"
+                    :id="`goal-${index}`"
+                    type="checkbox"
+                    name="goal"
+                    v-model="goalModel"
+                    :value="value"
+                    class="w-4 h-4 text-blue-500 bg-gray-100 border-gray-300 rounded checked:bg-blue-500 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    :for="`goal-${index}`"
+                    class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >{{ value }}</label
+                  >
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label
+                for="about"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Could you tell us more about you and your objectives ?</label
+              >
+
+              <textarea
+                id="about"
+                v-model="aboutModel"
+                name="about"
+                rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..."
+              ></textarea>
+            </div>
             <button
               class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               @click="submit"
-              :disabled="!canSubmit || status.isLoading"
-              :class="{ 'cursor-not-allowed opacity-70': !canSubmit || status.isLoading }"
+              :disabled="status.isLoading"
+              :class="{ 'cursor-not-allowed opacity-70': status.isLoading }"
             >
               <svg
                 v-if="status.isLoading"
@@ -72,34 +216,50 @@
 
 <script lang="ts" setup>
 import { User } from '@utils/auth';
-import { getUser } from '@stores/auth';
-import { upsertProfile, profile } from '@stores/profile';
-import { computed, onMounted, ref } from 'vue';
-import { useStore, useVModel } from '@nanostores/vue';
-// const $profile = useStore(profile);
+import { upsertProfile, profile, profileData, goalChoices } from '@stores/profile';
+import { onMounted, ref } from 'vue';
+import { useVModel } from '@nanostores/vue';
+
+// const firstname = useVModel(profileData, 'firstname');
+const {
+  full_nameModel,
+  genderModel,
+  dobModel,
+  educationModel,
+  computer_xpModel,
+  goalModel,
+  aboutModel
+} = useVModel(profileData, [
+  'full_name',
+  'gender',
+  'dob',
+  'education',
+  'dob',
+  'computer_xp',
+  'goal',
+  'about'
+]);
 
 const user = ref<User | null>(null);
-
-const username = ref('');
 const xp = ref(0);
 const status = ref({ error: '', success: false, isLoading: false });
-const canSubmit = computed(() => username.value.trim() !== '');
-
+const goalsList = ref<string[]>();
 onMounted(async () => {
   const userProfile = profile.get();
-
+  const goalsListRes = await goalChoices();
+  goalsList.value = goalsListRes;
   user.value = userProfile;
-  username.value = userProfile?.user_metadata.username;
   xp.value = userProfile?.user_metadata.xp;
 });
 
 const submit = async (e: Event) => {
   e.preventDefault();
 
+  const data = profileData.get();
   status.value = { error: '', success: false, isLoading: true };
 
   try {
-    const { error } = await upsertProfile({ id: user.value?.id, username: username.value });
+    const { error } = await upsertProfile(data);
 
     if (error?.message) {
       status.value = {
--- src/stores/profile.ts
+++ src/stores/profile.ts
@@ -1,20 +1,42 @@
-import { action } from 'nanostores';
+import { action, map } from 'nanostores';
 import { persistentAtom } from '@nanostores/persistent';
 import { supabase, User } from '@utils/auth';
 import { UserAppMetadata } from '@supabase/supabase-js';
 
 export interface Profile {
   id?: string;
+  email?: string;
   updated_at?: Date;
   username?: string;
   xp?: number;
+  full_name?: string;
+  gender?: string;
+  education?: string;
+  dob?: Date;
+  computer_xp?: string;
+  goal?: string[];
+  about?: string;
 }
+
 const primitive = {
   encode: JSON.stringify,
   decode: JSON.parse
 };
 export const isConnected = persistentAtom<boolean>('isConnected', false, primitive);
 export const profile = persistentAtom<User | null>('user', null, primitive);
+
+const defaultProfile = {
+  full_name: '',
+  gender: '',
+  education: '',
+  dob: undefined,
+  computer_xp: '',
+  goal: [],
+  about: '',
+  ...profile.get()?.user_metadata
+};
+
+export const profileData = map<Profile>(defaultProfile);
 
 export const increasePoints = action(profile, 'increasePoints', (store, add) => {
   const user = store.get();
@@ -56,5 +78,15 @@ export const upsertProfile = async (profile: Profile) => {
     return { data, error };
   } catch (error) {
     return { data: null, error };
+  }
+};
+
+export const goalChoices = async (): Promise<string[]> => {
+  try {
+    const { data: goals, error } = await supabase.from('goals').select('label').eq('active', true);
+
+    return goals?.map((g) => g.label) || [];
+  } catch (error) {
+    return [];
   }
 };
