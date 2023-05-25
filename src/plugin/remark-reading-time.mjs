e254e9f226b178b65a7447cd553e17a40b1ee8d2
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-05-25 14:19:44 +1100

    feat(minuteRead)

--- src/plugin/remark-reading-time.mjs
+++ src/plugin/remark-reading-time.mjs
@@ -1,13 +1,16 @@
 import getReadingTime from 'reading-time';
 import { toString } from 'mdast-util-to-string';
+/*
+Not working it this project folder structure
+*/
 
 export function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
     const readingTime = getReadingTime(textOnPage);
+
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
     data.astro.frontmatter.minutesRead = readingTime.text;
-    // console.log('remarkReadingTime', data.astro.frontmatter);
   };
 }
