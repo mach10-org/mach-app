5a45a247477eaae16b58091b7da244bbd7dd4a6c
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-05 15:05:41 +1100

    contact vue

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form action="#" class="space-y-8">
+    <div>
+      <OField labelFor="email">
+        <template #label> Your email address <span class="text-xs text-gray-500">(So we can reply to you)</span> </template>
+        <OInput placeholder="" type="email" name="contact[email]" id="email" required size="large" />
+      </OField>
+    </div>
+    <div>
+      <OField labelFor="topic" label="Topic">
+        <OSelect id="topic" name="contact[topic]" placeholder="Select a topic" size="large">
+          <option value="US">I have a question</option>
+          <option value="CA">I'd like to offer help</option>
+          <option value="FR">I have an issue with my account</option>
+        </OSelect>
+      </OField>
+    </div>
+    <div>
+      <OField labelFor="subject" label="Subject">
+        <OInput placeholder="Let us know how we can help you" type="text" name="contact[subject]" id="subject" required size="large" />
+      </OField>
+    </div>
+    <div class="sm:col-span-2">
+      <OField labelFor="message" label="Your message"> <OInput rows="5" placeholder="Leave a comment..." type="textarea" name="contact[message]" id="message" required size="large" /> </OField>
+    </div>
+    <div>
+      <OButton type="submit" variant="primary" size="large" name="contact_submit">Send message</OButton>
+    </div>
+  </form>
+</template>
+
+<script lang="ts" setup>
+import { OButton, OInput, OField, OSelect } from '@oruga-ui/oruga-next';
+</script>
