59a1f5ebf9f7e74dffa53914339580366ed4967e
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-05 13:16:14 +1100

    navbar and footer

--- src/components/Auth/LoginStatus.vue
+++ src/components/Auth/LoginStatus.vue
@@ -19,27 +19,17 @@
       id="user-dropdown"
       :class="{ hidden: !showMenu }"
     >
-      <div class="px-4 py-3">
-        <span id="userName" class="block text-sm text-gray-900 dark:text-white">{{
-          $profile?.email
-        }}</span>
-        <span
-          id="xpPoints"
-          class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400"
-          >{{ $profile?.user_metadata?.xp }} Points</span
-        >
+      <LoginStatusMenu />
+      <!-- <div class="px-4 py-3">
+        <span id="userName" class="block text-sm text-gray-900 dark:text-white">{{ $profile?.email }}</span>
+        <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">{{ $profile?.user_metadata?.xp }} Points</span>
       </div>
       <ul class="py-2" aria-labelledby="user-menu-button">
         <li>
-          <a
-            :href="`${BASE_URL}profile/`"
-            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-            >Profil</a
-          >
+          <a :href="`${BASE_URL}profile/`" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
         </li>
         <li>
           <button
-            id="logout"
             class="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
             type="button"
             @click="handleLogoutClick"
@@ -47,20 +37,11 @@
             Sign out
           </button>
         </li>
-      </ul>
+      </ul> -->
     </div>
   </div>
 
-  <OButton
-    v-else
-    id="isNotLoggedIn"
-    tag="a"
-    :href="`${BASE_URL}login/`"
-    class="mr-5"
-    variant="primary"
-  >
-    Login
-  </OButton>
+  <OButton v-else id="isNotLoggedIn" tag="a" :href="`${BASE_URL}login/`" class="mr-5" variant="primary"> Login </OButton>
 </template>
 <script lang="ts" setup>
 import { UserCircleIcon } from '@heroicons/vue/24/outline';
@@ -68,7 +49,8 @@ import { getUser, logout } from '@stores/auth';
 import { isConnected, profile, removeUser, setUser } from '@stores/profile';
 import { useStore } from '@nanostores/vue';
 import { onMounted, ref } from 'vue';
-import { OButton } from "@oruga-ui/oruga-next";
+import { OButton } from '@oruga-ui/oruga-next';
+import LoginStatusMenu from './LoginStatusMenu.vue';
 const $isConnected = useStore(isConnected);
 const $profile = useStore(profile);
 const BASE_URL = import.meta.env.BASE_URL;
---
+++ src/components/Auth/LoginStatusMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="px-4 py-3">
+    <span id="userName" class="block text-sm text-gray-900 dark:text-white">{{ $profile?.email }}</span>
+    <span class="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">{{ $profile?.user_metadata?.xp }} Points</span>
+  </div>
+  <ul class="py-2" aria-labelledby="user-menu-button">
+    <li>
+      <a :href="`${BASE_URL}profile/`" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
+    </li>
+    <li>
+      <button class="block text-left w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" type="button" @click="handleLogoutClick">
+        Sign out
+      </button>
+    </li>
+  </ul>
+</template>
+<script lang="ts" setup>
+import { logout } from '@stores/auth';
+import { profile, removeUser } from '@stores/profile';
+import { useStore } from '@nanostores/vue';
+const $profile = useStore(profile);
+const BASE_URL = import.meta.env.BASE_URL;
+
+const handleLogoutClick = () => {
+  logout();
+  removeUser();
+  window.location.assign(`${import.meta.env.BASE_URL}`);
+};
+</script>
