cfb8f56db119b0b00f0a1b0b784de9be3cc478e8
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-06-07 12:00:26 +1100

    Import images

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,40 +3,33 @@ import remarkMermaid from 'astro-diagram/remark-mermaid';
 import AutoImport from 'astro-auto-import';
 import remarkGFM from 'remark-gfm';
 import { remarkReadingTime } from './src/plugin/remark-reading-time.mjs';
-
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-// https://astro.build/config
 import react from '@astrojs/react';
+
+// https://astro.build/config
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mach10-org.github.io/',
   base: process.env.NODE_ENV === 'development' ? '' : '/mach-app/',
   trailingSlash: 'ignore',
-
   markdown: {
     remarkPlugins: [remarkMermaid, remarkGFM, remarkReadingTime],
     gfm: true,
     drafts: false,
     extendDefaultPlugins: true
   },
-  integrations: [
-    AutoImport({
-      imports: [
-        {
-          './src/components/Quiz': ['Quiz']
-        }
-      ]
-    }),
-    tailwind(),
-    mdx(),
-    vue({
-      jsx: true
-    }),
-    sitemap(),
-    react()
-  ]
+  integrations: [AutoImport({
+    imports: [{
+      './src/components/Quiz': ['Quiz']
+    }]
+  }), tailwind(), mdx(), vue({
+    jsx: true
+  }), sitemap(), react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
