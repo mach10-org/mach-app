2a4391a4acdb7c8797452bf66cc1aace0c53f89f
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-09 16:25:16 +1100

    journey link

--- src/components/Auth/LoginStatusMenu.vue
+++ src/components/Auth/LoginStatusMenu.vue
@@ -5,6 +5,9 @@
   </div>
   <ul class="py-2" aria-labelledby="user-menu-button">
     <li>
+      <a :href="`${BASE_URL}journey/`" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Journey</a>
+    </li>
+    <li>
       <a :href="`${BASE_URL}profile/`" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Profil</a>
     </li>
     <li>
