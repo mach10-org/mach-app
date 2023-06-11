85dfe2d9e0ca0c711bc050bb5e17fdcf54f43d18
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-12 09:51:52 +1100

    contact input size

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -3,12 +3,12 @@
     <div>
       <OField labelFor="email">
         <template #label> Your email address <span class="text-xs text-gray-500">(So we can reply to you)</span> </template>
-        <OInput placeholder="" type="email" name="contact[email]" id="email" required size="large" />
+        <OInput placeholder="" type="email" name="contact[email]" id="email" required />
       </OField>
     </div>
     <div>
       <OField labelFor="topic" label="Topic">
-        <OSelect id="topic" name="contact[topic]" placeholder="Select a topic" size="large">
+        <OSelect id="topic" name="contact[topic]" placeholder="Select a topic" size="medium">
           <option value="US">I have a question</option>
           <option value="CA">I'd like to offer help</option>
           <option value="FR">I have an issue with my account</option>
@@ -17,14 +17,14 @@
     </div>
     <div>
       <OField labelFor="subject" label="Subject">
-        <OInput placeholder="Let us know how we can help you" type="text" name="contact[subject]" id="subject" required size="large" />
+        <OInput placeholder="Let us know how we can help you" type="text" name="contact[subject]" id="subject" required />
       </OField>
     </div>
     <div class="sm:col-span-2">
-      <OField labelFor="message" label="Your message"> <OInput rows="5" placeholder="Leave a comment..." type="textarea" name="contact[message]" id="message" required size="large" /> </OField>
+      <OField labelFor="message" label="Your message"> <OInput rows="5" placeholder="Leave a comment..." type="textarea" name="contact[message]" id="message" required /> </OField>
     </div>
     <div>
-      <OButton type="submit" variant="primary" size="large" name="contact_submit">Send message</OButton>
+      <OButton type="submit" variant="primary" name="contact_submit">Send message</OButton>
     </div>
   </form>
 </template>
