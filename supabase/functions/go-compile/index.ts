defd34afb41fc3bdfb57c4d5ba2bc5d4334ff892
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-15 15:11:34 +1100

    feat(compiler FMT check)

--- supabase/functions/go-compile/index.ts
+++ supabase/functions/go-compile/index.ts
@@ -7,19 +7,46 @@ serve(async (req) => {
   }
 
   try {
-    const { code } = await req.json();
+    const { code, compile, fmt } = await req.json();
     // const goCode = 'package main\n\nimport "fmt";\n\nfunc main() {\n  fmt.Println("Hello, Guillaume")\n}';
-    const form = new FormData();
-    form.append('version', '2');
-    form.append('body', code);
-    const options = {
-      method: 'POST',
-      body: form,
-      headers: new Headers({ 'User-Agent': 'mach10-app-go-compiler' })
+    let data = {
+      events: {},
+      formated: ''
     };
 
-    const crudResponse = await fetch('https://go.dev/_/compile?backend=', options);
-    const data = await crudResponse.json();
+    if (compile) {
+      const form = new FormData();
+      form.append('version', '2');
+      form.append('body', code);
+      const options = {
+        method: 'POST',
+        body: form,
+        headers: new Headers({ 'User-Agent': 'mach10-app-go-compiler' })
+      };
+      const crudResponse = await fetch('https://go.dev/_/compile?backend=', options);
+      const result = await crudResponse.json();
+      data.events = result;
+    }
+
+    if (fmt) {
+      const headers = new Headers();
+      const urlencoded = new URLSearchParams();
+
+      headers.append('Content-Type', 'application/x-www-form-urlencoded');
+      urlencoded.append('imports', 'true');
+      urlencoded.append('body', code);
+
+      const options = {
+        method: 'POST',
+        headers: headers,
+        body: urlencoded
+      };
+
+      const crudResponse = await fetch('https://go.dev/_/fmt?backend=', options);
+      const result = await crudResponse.json();
+      data.formated = result;
+    }
+
     return new Response(JSON.stringify(data), {
       headers: { ...corsHeaders, 'Content-Type': 'application/json' },
       status: 200
