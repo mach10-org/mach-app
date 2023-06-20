e95106a6869a4344079f892bdd501a73ef8f3d55
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-21 10:56:58 +1100

    Notifification library

---
+++ src/constants/localize.ts
@@ -0,0 +1,13 @@
+export const locales = {
+  notifications: {
+    newsletter: {
+      title: 'Newsletter',
+      email_saved: `Your email has been saved !`,
+      email_already_saved: `You have already registered this email ! We'll be in touch !`,
+      not_valid_email: 'The email address is not valid'
+    }
+  },
+  errors: {
+    default: `An error has occured ! Please try again later !`
+  }
+};
---
+++ src/utils/notify.ts
@@ -0,0 +1,51 @@
+import Notify from 'simple-notify';
+import { notifyStatus } from 'simple-notify/dist/types';
+
+export interface ToastProps {
+  status?: notifyStatus;
+  autoclose?: boolean;
+  title?: string;
+  text?: string;
+  autotimeout?: number;
+}
+
+let toast: Notify;
+
+const closeIcon =
+  'svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>';
+const customIcon = {
+  success:
+    '<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200"><svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg><span class="sr-only">Check icon</span></div>',
+  error:
+    '<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200"><svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg><span class="sr-only">Error icon</span></div>',
+  warning:
+    '<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200"><svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path></svg><span class="sr-only">Warning icon</span></div>',
+  info: '<div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8  bg-blue-100 rounded-lg dark:text-blue-300 dark:bg-blue-900"><svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"></path></svg><span class="sr-only">Info icon</span></div>'
+};
+
+const showToast = ({ status = 'success', autoclose = true, title = '', text = '', autotimeout = 3500 }: ToastProps) => {
+  toast = new Notify({
+    status,
+    title,
+    text,
+    effect: 'slide',
+    speed: 300,
+    customClass: 'toast-mach',
+    customIcon: customIcon[status],
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: autoclose || status === 'success' ? true : false,
+    autotimeout: autotimeout || status === 'success' ? 3500 : 10000,
+    gap: 20,
+    distance: 20,
+    type: 1,
+    position: 'right bottom'
+  });
+};
+
+const closeToast = () => {
+  toast?.close();
+};
+
+export { toast, showToast, closeToast };
+export type { notifyStatus };
