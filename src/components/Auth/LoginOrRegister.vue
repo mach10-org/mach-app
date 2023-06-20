00b40c03d7cddab8eca1652a3f528bc559110609
Guillaume Bartolini <g.bartolini@ncit.nc>
2023-06-20 16:59:22 +1100

    new avatar pixelart

--- src/components/Auth/LoginOrRegister.vue
+++ src/components/Auth/LoginOrRegister.vue
@@ -34,15 +34,7 @@
       <p class="text-center my-8 text-text-muted">OR</p>
       <div class="px-6">
         <button class="github-login" @click="signInWithGitHub" :disabled="statusOAuth.isLoading">
-          <svg
-            v-if="statusOAuth.isLoading"
-            aria-hidden="true"
-            role="status"
-            class="self-center animate-spin"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg v-if="statusOAuth.isLoading" aria-hidden="true" role="status" class="self-center animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
               fill="currentColor"
@@ -92,8 +84,6 @@ const signInWithGitHub = async (e: Event) => {
 
   try {
     const { data, error } = await signInWithOAuth('github');
-    console.log('error', error);
-    console.log('data', data);
   } catch (error) {
     console.error('error', error);
   }
--- src/components/Auth/LoginStatus.vue
+++ src/components/Auth/LoginStatus.vue
@@ -2,14 +2,14 @@
   <div id="isLoggedIn" v-if="$isConnected" class="relative ml-5 mr-2">
     <button
       type="button"
-      class="transition-colors rounded-lg p-2 text-center inline-flex items-center focus:ring-4 focus:ring-link/70 focus:outline-none hover:text-link"
+      class="transition-colors p-0 rounded-full text-center inline-flex items-center focus:ring-4 focus:ring-link/70 focus:outline-none hover:text-link"
       id="user-menu-button"
       :aria-expanded="isOpen"
       aria-controls="user-dropdown"
       @click="isOpen = !isOpen"
     >
       <span class="sr-only">Open user menu</span>
-      <UserCircleIcon class="h-7 h-7" />
+      <img :src="avatar" class="h-10 h-10 rounded-full" />
     </button>
     <!-- Dropdown menu -->
     <div
@@ -24,7 +24,6 @@
   <OButton v-else id="isNotLoggedIn" tag="a" :href="`${BASE_URL}login/`" class="mr-5" variant="primary"> Login </OButton>
 </template>
 <script lang="ts" setup>
-import { UserCircleIcon } from '@heroicons/vue/24/solid';
 import { getUser, logout } from '@stores/auth';
 import { isConnected } from '@stores/profile';
 import { useStore } from '@nanostores/vue';
@@ -32,9 +31,10 @@ import { onMounted, ref } from 'vue';
 import { OButton } from '@oruga-ui/oruga-next';
 import LoginStatusMenu from './LoginStatusMenu.vue';
 import { getCourseTaken } from '@stores/courses';
+import avatar from '@assets/img/avatar.png?url';
+
 const $isConnected = useStore(isConnected);
 const BASE_URL = import.meta.env.BASE_URL;
-
 const isOpen = ref(false);
 
 onMounted(async () => {
