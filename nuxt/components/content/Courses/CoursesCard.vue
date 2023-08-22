db0e275dfda84c1c767cd77c2d92228e98e4236c
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-08-22 17:01:23 +1100

    Fix course card design

--- nuxt/components/content/Courses/CoursesCard.vue
+++ nuxt/components/content/Courses/CoursesCard.vue
@@ -2,17 +2,17 @@
   <li>
     <nuxt-link
       :to="href"
-      class="link group h-full flex flex-col overflow-hidden border border-$border-input rounded bg-$background-base no-underline drop-shadow-lg transition transition duration-500 hover:drop-shadow-2xl"
+      class="group h-full flex flex-col overflow-hidden border border-$border-input rounded bg-$background-base no-underline drop-shadow-lg transition duration-500 hover:drop-shadow-2xl"
     >
       <nuxt-img class="h-[223px] w-full object-cover" :src="`/img/${preview}`" alt="" sizes="xs:100vw sm:100vw md:100vw lg:360px" />
       <div class="flex flex-1 flex-col px-4 py-3">
-        <h2 class="mb-1 text-xl font-extrabold text-$link transition-colors lg:text-2xl group-hover:text-$link-hover">
+        <h2 class="mb-1 text-xl font-extrabold text-$link underline underline-dashed transition-colors duration-500 lg:text-2xl group-hover:(text-$link-hover underline-solid)">
           {{ title }}
         </h2>
-        <p class="text-text-base m-0 flex-1 leading-6 transition-colors">
+        <p class="m-0 flex-1 text-base leading-6 text-$text-base">
           {{ description }}
         </p>
-        <span v-if="totalHours" class="text-text-muted mt-2 block text-sm transition-colors">
+        <span v-if="totalHours" class="mt-2 block text-sm text-$text-muted">
           {{ totalHours }} hours
         </span>
       </div>
