264b6d7daa35aa9d2eb14f48bd9edf12e6ba2c9b
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-08-25 15:49:12 +1100

    Start course index page

---
+++ nuxt/pages/courses/[slug]/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="data">
+    <ContentRenderer :value="data" class="prose lg:text-xl" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const { locale } = useI18n()
+
+const { data } = await useAsyncData(`course-${route.params.slug}-index`, () =>
+  queryContent(
+    `/${locale.value}/courses/${route.params.slug}/00-index`,
+  ).findOne(),
+)
+</script>
