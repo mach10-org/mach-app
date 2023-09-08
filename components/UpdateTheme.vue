3091de7e35663c8a9ecabf085bacbf75e88738b5
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-09-08 14:49:47 +1100

    Fix generate

--- components/UpdateTheme.vue
+++ components/UpdateTheme.vue
@@ -3,11 +3,13 @@
 <script setup lang="ts">
 import { useUtilsStore } from '~/stores/utils'
 
-const theme = useThemeOverridesVars()
-const utils = useUtilsStore()
+onMounted(() => {
+  const theme = useThemeOverridesVars()
+  const utils = useUtilsStore()
 
-watch(theme, (val) => {
-  utils.themeOverrides = val
-}, { immediate: true })
+  watch(theme, (val) => {
+    utils.themeOverrides = val
+  }, { immediate: true })
+})
 
 </script>
