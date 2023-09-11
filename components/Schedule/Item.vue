46746ae68ba30a56ae9894fd413c1f6469084b08
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-09-12 10:34:53 +1100

    Form validation

--- components/Schedule/Item.vue
+++ components/Schedule/Item.vue
@@ -1,5 +1,5 @@
 <template>
-  <n-form-item :show-label="false">
+  <n-form-item :show-label="false" :show-feedback="false">
     <div class="min-h-$n-blank-height w-full flex items-start">
       <div class="flex items-center pt-[6px]">
         <n-switch v-model:value="valueRef.isChecked" />
@@ -8,23 +8,40 @@
         </span>
       </div>
       <n-dynamic-input v-if="valueRef.isChecked" v-model:value="valueRef.list" :min="1" :on-create="onCreate" class="ml-auto !w-auto">
-        <template #default="{value}">
-          <n-time-picker
-            ref="startRef"
-            v-model:formatted-value="value.start"
-            format="HH:mm"
-            :minutes="5"
-            :seconds="[0]"
-            :placeholder="$t('pages.schedule.startTime')"
-            class="mr-2"
-          />
-          <n-time-picker
-            v-model:formatted-value="value.end"
-            format="HH:mm"
-            :minutes="5"
-            :seconds="[0]"
-            :placeholder="$t('pages.schedule.endTime')"
-          />
+        <template #default="{index, value}">
+          <div class="schedule-period-picker flex items-start">
+            <n-form-item
+              :show-label="false"
+              ignore-path-change
+              :path="`items[${valueRef.day}]list[${index}].start`"
+              :rule="startRule"
+            >
+              <n-time-picker
+                ref="startRef"
+                v-model:formatted-value="value.start"
+                format="HH:mm"
+                :minutes="5"
+                :seconds="[0]"
+                :placeholder="$t('pages.schedule.startTime')"
+                class="mr-2 w-38"
+              />
+            </n-form-item>
+            <n-form-item
+              :show-label="false"
+              ignore-path-change
+              :path="`items[${valueRef.day}]list[${index}].end`"
+              :rule="endRule"
+            >
+              <n-time-picker
+                v-model:formatted-value="value.end"
+                format="HH:mm"
+                :minutes="5"
+                :seconds="[0]"
+                :placeholder="$t('pages.schedule.endTime')"
+                class="w-38"
+              />
+            </n-form-item>
+          </div>
         </template>
       </n-dynamic-input>
     </div>
@@ -48,8 +65,58 @@ interface Props {
 }
 const props = defineProps<Props>()
 
+const dayjs = useDayjs()
+const i18n = useI18n()
+
 const valueRef = toRef(props, 'value')
 const startRef = ref<TimePickerInst | null>(null)
+
+const checkPeriod = (start: string, end: string) => {
+  const startDay = dayjs(start, 'HH:mm').second(0).millisecond(0)
+  const endDay = dayjs(end, 'HH:mm').second(0).millisecond(0)
+
+  if (startDay.isAfter(endDay)) {
+    return false
+  }
+
+  return true
+}
+
+const startRule = {
+  trigger: 'blur',
+  validator (rule: unknown, value: string) {
+    if (value === null) {
+      return new Error(i18n.t('pages.schedule.startTimeRequired'))
+    }
+
+    const matches = rule.field.match(/list\[(\d+)\]/)
+    const index = matches ? parseInt(matches[1]) : 0
+
+    if (!checkPeriod(value, valueRef.value.list[index].end ?? '')) {
+      return new Error(i18n.t('pages.schedule.startTimeAfterEndTime'))
+    }
+
+    return true
+  },
+}
+
+const endRule = {
+  trigger: 'blur',
+  validator (rule: unknown, value: string) {
+    if (value === null) {
+      return new Error(i18n.t('pages.schedule.endTimeRequired'))
+    }
+
+    const matches = rule.field.match(/list\[(\d+)\]/)
+    const index = matches ? parseInt(matches[1]) : 0
+
+    if (!checkPeriod(valueRef.value.list[index].start ?? '', value)) {
+      return new Error(i18n.t('pages.schedule.endTimeBeforeStartTime'))
+    }
+
+    return true
+  },
+}
 
 const onCreate = () => {
   return {
@@ -61,8 +128,6 @@ const onCreate = () => {
 if (valueRef.value.list.length === 0) {
   valueRef.value.list.push(onCreate())
 }
-
-const dayjs = useDayjs()
 
 const isChecked = computed(() => valueRef.value.isChecked)
 watch(isChecked, (value) => {
@@ -78,3 +143,9 @@ watch(isChecked, (value) => {
 })
 
 </script>
+
+<style>
+.schedule-period-picker .n-form-item-feedback.n-form-item-feedback--error {
+  max-width: 145px;
+}
+</style>
--- pages/schedule.vue
+++ pages/schedule.vue
@@ -8,8 +8,10 @@
       <div>
         <ScheduleTimezone @updated="save" />
         <div class="mt-5 border-t border-$border-input pt-5" />
-        <ScheduleItem v-for="(item, index) in items" :key="item.day" v-model:value="items[index]" />
-        <n-button type="primary" size="large" class="w-full" :loading="isSaving" @click="save">
+        <n-form ref="formRef" :model="model" class="space-y-3">
+          <ScheduleItem v-for="(item, index) in model.items" :key="item.day" v-model:value="model.items[index]" />
+        </n-form>
+        <n-button type="primary" size="large" class="mt-7 w-full" :loading="pending" @click="onSubmit(save)">
           Save
         </n-button>
       </div>
@@ -28,6 +30,49 @@ definePageMeta({
 const dayjs = useDayjs()
 const schedule = useScheduleStore()
 const profile = useProfileStore()
+
+interface Item {
+    day: number;
+    isChecked: boolean;
+    list: {
+        start: string;
+        end: string;
+    }[];
+}
+
+const isLoadingProfile = computed(() => profile.isLoading)
+
+const model = ref<{items: Item[]}>({ items: [] })
+
+const { formRef, onSubmit, pending } = useNaiveForm(model)
+
+const save = async () => {
+  const list: Array<{
+    day: number
+    start: string
+    end: string
+  }> = []
+
+  model.value.items.forEach((item) => {
+    if (item.isChecked) {
+      item.list.forEach((time) => {
+        if (!time.start || !time.end) { return }
+
+        // Convert to UTC
+        const start = dayjs(time.start, 'HH:mm').second(0).day(item.day).tz(profile.timezone, true).utc()
+        const end = dayjs(time.end, 'HH:mm').second(0).day(item.day).tz(profile.timezone, true).utc()
+
+        list.push({
+          day: start.isoWeekday(),
+          start: start.format('HH:mm:ss'),
+          end: end.format('HH:mm:ss'),
+        })
+      })
+    }
+  })
+
+  await schedule.save(list)
+}
 
 const getItems = () => {
   // Init
@@ -57,40 +102,8 @@ const getItems = () => {
   return items
 }
 
-const items = ref(getItems())
-
-const isSaving = ref(false)
-
-const save = async () => {
-  // TODO validation
-  isSaving.value = true
-
-  const list: Array<{
-    day: number
-    start: string
-    end: string
-  }> = []
-
-  items.value.forEach((item) => {
-    if (item.isChecked) {
-      item.list.forEach((time) => {
-        if (!time.start || !time.end) { return }
-
-        // Convert to UTC
-        const start = dayjs(time.start, 'HH:mm').second(0).day(item.day).tz(profile.timezone, true).utc()
-        const end = dayjs(time.end, 'HH:mm').second(0).day(item.day).tz(profile.timezone, true).utc()
-
-        list.push({
-          day: start.isoWeekday(),
-          start: start.format('HH:mm:ss'),
-          end: end.format('HH:mm:ss'),
-        })
-      })
-    }
-  })
-
-  await schedule.save(list)
-
-  isSaving.value = false
-}
+onMounted(async () => {
+  await until(isLoadingProfile).toBe(false)
+  model.value.items = getItems()
+})
 </script>
