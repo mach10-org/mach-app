6b99ff254d233d367a269fb1f5f6fa4928fc4154
Nicolas Hamelin <n.hamelin@ncit.nc>
2023-09-18 15:24:36 +1100

    Removed unused i18n keys

--- pages/courses/[slug]/[...lesson].vue
+++ pages/courses/[slug]/[...lesson].vue
@@ -55,7 +55,7 @@
         >
           <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z" />
         </svg>
-        <span class="sr-only">{{ $t('page.course.showNavigation') }}</span>
+        <span class="sr-only">{{ $t('pages.course.showNavigation') }}</span>
       </button>
       <n-drawer id="drawer-navigation" v-model:show="isTocActive" :width="320" placement="left" display-directive="show">
         <n-drawer-content closable>
